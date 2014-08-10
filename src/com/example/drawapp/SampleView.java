63844d142256bde2f5d617c1716b96ba50b5d7c5
mzkrelx <mizuki@relx.jp>
2014-08-10 14:38:14 +0900

    SampleViewを作成

---
+++ src/com/example/drawapp/SampleView.java
@@ -0,0 +1,23 @@
+package com.example.drawapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class SampleView extends View {
+
+	private Paint paint = new Paint();
+	
+	public SampleView(Context context) {
+		super(context);
+		setBackgroundColor(Color.WHITE);
+	}
+	
+	@Override
+	public void onDraw(Canvas canvas) {
+		canvas.drawText("Hello", 100, 100, paint);
+	}
+
+}
